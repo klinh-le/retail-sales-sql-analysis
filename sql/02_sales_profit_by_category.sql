cde2a621addd3cf54800c2586b1992881597551f
Linh Le <klle6.257@gmail.com>
2026-09-09 00:09:59 -0400

    Remove stray files

--- sql/02_sales_profit_by_category.sql
+++
@@ -1,4 +0,0 @@
-SELECT SUM(sales) AS total_sale, SUM(profit) AS total_profit, ROUND(SUM(profit) / SUM(sales) * 100, 2) AS profit_margin, category, sub_category
-FROM orders
-GROUP BY category, sub_category
-ORDER BY profit_margin;
